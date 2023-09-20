51d8b07569db78ef8a02f86018816b3e31c44358
Alex Koen <git@koen.ca>
2023-09-20 10:14:53 -0700

    doc: add examples dir

---
+++ examples/imprinted-boxes.py
@@ -0,0 +1,16 @@
+"""Making the stellarmesh logo."""
+import logging
+
+import build123d as bd
+import stellarmesh as sm
+
+logging.getLogger("stellarmesh").setLevel(logging.INFO)
+
+b1 = bd.Solid.make_box(10, 10, 10)
+b2 = b1.transformed(offset=(0, 10, 0))
+b3 = b1.transformed(offset=(0, 5, 10))
+
+cmp_initial = bd.Compound.make_compound([b1, b2, b3])
+solids = cmp_initial.solids()
+geom = sm.Geometry(solids, material_names=[""] * len(solids))
+geom_imprinted = geom.imprint()
