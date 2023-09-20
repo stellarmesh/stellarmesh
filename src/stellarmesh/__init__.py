5e85ea33984c2ee83c70fc6e8e95b083c89bf8f8
Alex Koen <git@koen.ca>
2023-09-20 10:00:03 -0700

    fix: module installation

--- src/stellarmesh/__init__.py
+++ src/stellarmesh/__init__.py
@@ -1,4 +1,4 @@
 """GMSH wrapper and DAGMC geometry creator."""
-from geometry import Geometry  # noqa: F401
-from mesh import Mesh  # noqa: F401
-from moab import MOABModel  # noqa: F401
+from .geometry import Geometry  # noqa: F401
+from .mesh import Mesh  # noqa: F401
+from .moab import MOABModel  # noqa: F401
