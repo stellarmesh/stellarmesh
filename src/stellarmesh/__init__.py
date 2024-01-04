9a93cd20cf91f734c138cf450f468f6eb0d9fa5a
Paul Romano <paul.k.romano@gmail.com>
2024-01-03 23:50:44 -0600

    Start adding tests for moab.py module

--- src/stellarmesh/__init__.py
+++ src/stellarmesh/__init__.py
@@ -1,4 +1,4 @@
 """GMSH wrapper and DAGMC geometry creator."""
 from .geometry import Geometry  # noqa: F401
 from .mesh import Mesh  # noqa: F401
-from .moab import MOABModel, DAGMCModel  # noqa: F401
+from .moab import MOABModel, DAGMCModel, DAGMCSurface, DAGMCVolume  # noqa: F401
