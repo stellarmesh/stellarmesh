c3727f0ee7e1e987175a2e4cc6400aa5a0637923
Paul Romano <paul.k.romano@gmail.com>
2024-01-07 15:50:47 -0600

    Fixes to make ruff happy

--- src/stellarmesh/__init__.py
+++ src/stellarmesh/__init__.py
@@ -1,4 +1,4 @@
 """GMSH wrapper and DAGMC geometry creator."""
 from .geometry import Geometry  # noqa: F401
 from .mesh import Mesh  # noqa: F401
-from .moab import MOABModel, DAGMCModel, DAGMCSurface, DAGMCVolume  # noqa: F401
+from .moab import DAGMCModel, DAGMCSurface, DAGMCVolume, MOABModel  # noqa: F401
