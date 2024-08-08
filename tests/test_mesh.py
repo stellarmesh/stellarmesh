e4ffa130a28b2742914ac4d8b71c80c490e871d5
Alex Koen <git@koen.ca>
2024-08-08 13:33:53 -0700

    feat: better meshing tests

--- tests/test_mesh.py
+++ tests/test_mesh.py
@@ -1,23 +1,56 @@
-import build123d as bd
+"""Meshing tests."""
+
+import filecmp
+from pathlib import Path
+
+import gmsh
 import pytest
 import stellarmesh as sm
 
+from .test_geometry import (
+    geom_bd_imprintedboxes,
+    geom_bd_layered_torus,
+    geom_bd_nestedspheres,
+    geom_bd_sphere,
+)
 
-@pytest.fixture
-def geom_bd_sphere():
-    solids = [bd.Solid.make_sphere(10.0)]
-    return sm.Geometry(solids, ["a"])
 
-
-def test_mesh_geometry_2d(geom_bd_sphere):
-    # sm.SurfaceMesh.from_geometry(geom_bd_sphere, 5, 5, dim=2)
+@pytest.mark.parametrize(
+    "model_name",
+    [
+        geom_bd_layered_torus.__name__,
+        geom_bd_nestedspheres.__name__,
+        geom_bd_sphere.__name__,
+        geom_bd_imprintedboxes.__name__,
+    ],
+)
+def test_mesh_output(model_name: str, request: pytest.FixtureRequest, tmp_path: Path):
+    """Ensure that generated meshes match benchmark .msh files."""
+    model = request.getfixturevalue(model_name)
+    msh_name = f"mesh_{model_name}.msh"
     mesh = sm.SurfaceMesh.from_geometry(
-        geom_bd_sphere,
+        model,
         sm.OCCSurfaceOptions(0.1, 10),
-        # sm.GMSHSurfaceOptions(5, 5, sm.GMSHSurfaceAlgo.FRONTAL_DELAUNAY_QUADS),
     )
-    mesh.write("out.msh")
+    msh_file = tmp_path / msh_name
+    mesh.write(str(msh_file))
+    cmp_file = Path(__file__).parent / "data" / msh_name
+
+    # For initial benmark (check meshes!)
+    init = False
+    if init:
+        mesh.write(str(cmp_file))
+        raise AssertionError()
+
+    assert filecmp.cmp(msh_file, cmp_file, shallow=False), "Mesh files differ"
 
 
-def test_mesh_geometry_3d(geom_bd_sphere):
-    sm.SurfaceMesh.from_geometry(geom_bd_sphere, 5, 5, dim=3)
+def test_mesh_geom_bd_imprintedboxes(geom_bd_imprintedboxes):
+    """Test imprinting."""
+    mesh = sm.SurfaceMesh.from_geometry(
+        geom_bd_imprintedboxes,
+        sm.OCCSurfaceOptions(0.1, 10),
+    )
+    with mesh:
+        surface_tags = gmsh.model.get_entities(2)
+        assert len(surface_tags) == 13
