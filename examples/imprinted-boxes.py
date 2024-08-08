b379f2ce2c9cc3a743b23609a7f06bfab65b5ee4
Alex Koen <git@koen.ca>
2024-08-08 13:40:16 -0700

    refactor: make Solid a Protocol

--- examples/imprinted-boxes.py
+++ examples/imprinted-boxes.py
@@ -11,6 +11,7 @@ b1 = bd.Solid.make_box(10, 10, 10)
 b2 = b1.transformed(offset=(0, 10, 0))
 b3 = b1.transformed(offset=(0, 5, 10))
 
+
 cmp_initial = bd.Compound.make_compound([b1, b2, b3])
 solids = cmp_initial.solids()
 geom = sm.Geometry(solids, material_names=[""] * len(solids))
