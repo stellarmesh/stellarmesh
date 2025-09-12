4f55f1806938408a669445df69e1ef758320c13d
akoen <git@koen.ca>
2025-09-12 16:51:12 -0700

    refactor: set version dynamically

--- docs/conf.py
+++ docs/conf.py
@@ -1,10 +1,4 @@
-# Configuration file for the Sphinx documentation builder.
-#
-# For the full list of built-in configuration values, see the documentation:
-# https://www.sphinx-doc.org/en/master/usage/configuration.html
-
-# -- Project information -----------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+"""Configuration file for the Sphinx documentation builder."""
 
 import os
 import sys
@@ -13,12 +7,9 @@ sys.path.append(os.path.abspath("../"))
 import stellarmesh
 
 project = "Stellarmesh"
-copyright = "2025, Stellarmesh Developers"
+copyright = "2025, Stellarmesh Developers"  # noqa: A001
 author = "Alex Koen"
-release = "1.0.0b1"
-
-# -- General configuration ---------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+release = stellarmesh.__version__
 
 extensions = [
     "sphinx_github_style",
@@ -31,29 +22,10 @@ top_level = "stellarmesh"
 linkcode_blob = "head"
 linkcode_url = r"https://github.com/stellarmesh/stellarmesh"
 linkcode_link_text = "Source"
-
+add_module_names = False
 templates_path = ["_templates"]
 exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 
-
-# -- Options for HTML output -------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
-
 html_theme = "shibuya"
-html_theme_options = {
-    "logo_only": True,
-    "prev_next_buttons_location": "both",
-    "style_external_links": False,
-    "style_nav_header_background": "#3c4142",
-    # Toc options
-    "collapse_navigation": True,
-    "sticky_navigation": True,
-    "navigation_depth": 2,
-    "includehidden": True,
-    "titles_only": False,
-}
-
 html_static_path = ["_static"]
 html_css_files = ["custom.css"]
-
-add_module_names = False
